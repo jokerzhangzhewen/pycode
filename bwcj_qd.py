361060e107d6dacd95f40fb5a0b8d1967d910151
Code.K <160496306+Code-KKK@users.noreply.github.com>
2024-03-08 09:50:25 +0800

    更新霸王茶姬签到登录失败问题

--- bwcj_qd.py
+++ bwcj_qd.py
@@ -39,21 +39,18 @@ file_url = 'https://mirror.ghproxy.com/https://raw.githubusercontent.com/Code-KK
 def check_environment(file_name):
     v, o, a = sys.version_info, platform.system(), platform.machine()
     print(f"Python版本: {v.major}.{v.minor}.{v.micro}, 操作系统类型: {o}, 处理器架构: {a}")
-    if (v.minor in [8,9,10,11]) and o.lower() in ['linux','windows'] and a.lower() in ['x86_64','amd64','aarch64']:
+    if (v.minor in [8,9,10,11]) and o.lower() in ['linux'] and a.lower() in ['x86_64','aarch64']:
         print("当前环境符合运行要求")
-        if o.lower() == 'windows':
-            file_name += '.pyd'
-            main_run(file_name, v.minor, o.lower(), a.lower())
         if o.lower() == 'linux':
             file_name += '.so'
             main_run(file_name, v.minor, o.lower(), a.lower())
     else:
         if not (v.minor in [8,9,10,11]):
             print("不符合运行要求: Python版本不是 3.8 ~ 3.11")
-        if not (o.lower() in ['linux','windows']):
-            print(f"不符合运行要求: 操作系统类型[{o}] 支持：Linux或Window")
-        if not (a.lower() in ['x86_64','amd64','aarch64']):
-            print(f"不符合运行要求: 当前处理器架构[{a}] 支持：x86_64 amd64 aarch64")
+        if not (o.lower() in ['linux']):
+            print(f"不符合运行要求: 操作系统类型[{o}] 支持：Linux")
+        if not (a.lower() in ['x86_64','aarch64']):
+            print(f"不符合运行要求: 当前处理器架构[{a}] 支持：x86_64 aarch64")
 
 def main_run(file_name, py_v, os_info, cpu_info):
     if os.path.exists(file_name):
